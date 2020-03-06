e30ff705c1deda9b74d104988f001e3f5e320c38
cooldice <iamlegna@gmail.com>
2020-03-06 16:19:43 +0900

    code refactoring

--- RepositoryLib/Log.cs
+++ RepositoryLib/Log.cs
@@ -6,36 +6,42 @@ namespace RepositoryLib
     {
         public enum LOGLEVEL
         {
-            LOGLEVEL_NONE = 0,
-            LOGLEVEL_TRACE,
-            LOGLEVEL_DEBUG,
-            LOGLEVEL_ERROR,
-            LOGLEVEL_MAX
+            NONE = 0,
+            TRACE,
+            DEBUG,
+            ERROR,
+            EXCEPTION,
+            MAX
         };
 
-        static public LOGLEVEL LogLevel { get { return logLevel; } set { logLevel = value;  } } 
+        static public LOGLEVEL LogLevel { get { return _logLevel; } set { _logLevel = value;  } } 
         static public void Debug(string format, params object[] args)
         {
-            string log = String.Format("[DEBUG], " + format, args);
-            Console.WriteLine(log);
+            log(LOGLEVEL.DEBUG, format, args);
         }
         static public void Error(string format, params object[] args)
         {
-            string log = String.Format("[ERROR], " + format, args);
-            Console.WriteLine(log);
+            log(LOGLEVEL.ERROR, format, args);
         }
         static public void Trace(string format, params object[] args)
         {
-            string log = String.Format("[TRACE], " + format, args);
-            Console.WriteLine(log);
+            log(LOGLEVEL.TRACE, format, args);
         }
 
         static public void Exception(Exception e)
         {
-            string log = String.Format("[EXCEPTION]" + e.ToString());
+            log(LOGLEVEL.EXCEPTION, "{0}", e.ToString());
+        }
+
+        static private void log(LOGLEVEL loglevel, string format, params object[] args)
+        {
+            if (loglevel < LogLevel)
+                return;
+
+            string log = String.Format("[" + loglevel.ToString() + "], " + format, args);
             Console.WriteLine(log);
         }
 
-        static private LOGLEVEL logLevel = LOGLEVEL.LOGLEVEL_NONE;
+        static private LOGLEVEL _logLevel = LOGLEVEL.TRACE;
     }
 }
